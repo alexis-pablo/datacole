371c931ff57ca3ee4b0b2b52222353ec0246bbad
alexis-pablo <alexispijp@gmail.com>
2020-12-21 15:47:52 -0500

    initial commit

---
+++ application/views/fixed_button.php
@@ -0,0 +1,6 @@
+<div id="fixed_button" class="fixed-action-btn clearfix d-xl-none z-index">
+	<a class="btn-floating btn-lg btn-primary active">
+	  	<i class="fas fa-pencil-alt"></i>
+	</a>
+	<ul id="cliente"></ul>
+</div>
